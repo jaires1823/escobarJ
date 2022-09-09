838602b0fefeaefd4ead0e4107afd636c3afb6da
jair escobar <jaires1823@gmail.com>
2022-09-09 14:29:54 -0500

    3ra version en c++

--- suma.cpp
+++ suma.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main()
 {
-	float A,B,C;
-	cin>>A>>B;
-	C=A+B;
-	cout<<C;
+	float x,y,z;
+	cout<<"Ingrese 2 numeros separados de espacio x y :";
+	cin>>x>>y;
+	z=x+y;
+	cout<<"El resultado es :";
+	cout<<z<<endl;
 	return 0;
 } 
